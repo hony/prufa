86d77b37be169612124e1dcd6e80a9def9218974
Sigurdur Oli Gestsson <gestsson@gmail.com>
2011-04-22 08:57:55 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prufa::Application.config.secret_token = '53d3ebc8bd689b9d967ea1167d820444451dbc034d6138c63679b4cafcad3d796ab91aee30afa1a04a4c3b83aa4f1c54ed2e03b2d00ff81ed4523e1ecd32b29f'
